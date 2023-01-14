ba4af5d71e3efa5d53bfe54d1e37dc2954aea5f9
Maxime ABRIEL <mabriel@e1r5p20.clusters.42paris.fr>
2023-01-14 20:41:25 +0100

    push swap cleaner

--- test_push_swap/script.sh
+++ test_push_swap/script.sh
@@ -4,8 +4,9 @@ RED="\033[0;31m"
 GREEN="\033[0;32m"
 NC="\033[0m"
 
+make -C ..
+cp ../push_swap .
 c++ main.cpp
-
 
 for j in {1..25}
 
@@ -178,3 +179,34 @@ ret=$(($ret / 100))
 
 echo "500 average: $ret" 
 
+
+echo -n "leaks:"
+
+R=0
+for i in {1..101}
+do
+
+RAND=$(./a.out ${i})
+OUTPUT=$(valgrind --log-file="val" ./push_swap $RAND | ./checker $RAND)
+
+if [ "$OUTPUT" = "OK" ]; then
+
+else
+	echo "${RED}KO \n${RAND}${NC}"
+	RES=1
+fi
+
+if grep "no leaks are possible" val > /dev/null && grep "0 context" val > /dev/null
+then
+else echo "${RED}leaks with stack of size: ${i} \n ${RAND}" ; cat val; OQEFO=1 ; echo "${NC}"
+fi
+
+done
+
+if [ OQEFO ];
+then echo "${GREEN} OK${NC}"
+else
+fi
+
+make clean -C ..
+rm push_swap a.out
